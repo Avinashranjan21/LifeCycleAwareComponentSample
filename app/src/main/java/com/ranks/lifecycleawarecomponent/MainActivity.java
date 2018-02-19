c43e4c8597134f1a245f4638c9fc7dac92cc02f6
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-19 13:32:19 +0530

    lifecycle aware component added in MyComponent

--- app/src/main/java/com/ranks/lifecycleawarecomponent/MainActivity.java
+++ app/src/main/java/com/ranks/lifecycleawarecomponent/MainActivity.java
@@ -3,11 +3,14 @@ package com.ranks.lifecycleawarecomponent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ranks.lifecycleawarecomponent.lca_component.MyComponent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new MyComponent(this);
     }
 }
--- app/src/main/java/com/ranks/lifecycleawarecomponent/lca_component/MyComponent.java
+++ app/src/main/java/com/ranks/lifecycleawarecomponent/lca_component/MyComponent.java
@@ -1,12 +1,53 @@
 package com.ranks.lifecycleawarecomponent.lca_component;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MyComponent  {
+public class MyComponent implements LifecycleObserver {
+    private static final String TAG = "MyComponent";
 
     private Context mContext;
 
     public MyComponent(Context mContext) {
         this.mContext = mContext;
+        ((AppCompatActivity) this.mContext).getLifecycle().addObserver(this);
     }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void onCreateComponent() {
+        Log.d(TAG, "onCreateComponent: is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    public void onStartComponent() {
+        Log.d(TAG, "onStartComponent: is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void onResumeComponent() {
+        Log.d(TAG, "onResumeComponent: is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    public void onPauseComponent() {
+        Log.d(TAG, "onPauseComponent: is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void onStopComponent() {
+        Log.d(TAG, "onStopComponent: is called");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroyComponent() {
+        Log.d(TAG, "onDestroyComponent: is called");
+//        Always remove the observer associated with the component
+        ((AppCompatActivity) this.mContext).getLifecycle().removeObserver(this);
+    }
+
+
 }
