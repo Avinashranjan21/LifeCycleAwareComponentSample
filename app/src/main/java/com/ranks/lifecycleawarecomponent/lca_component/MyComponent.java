32b586f001d7bed4cab42c48d114cb6c4b3f52c7
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-19 13:19:46 +0530

    Initial commit

---
+++ app/src/main/java/com/ranks/lifecycleawarecomponent/lca_component/MyComponent.java
@@ -0,0 +1,12 @@
+package com.ranks.lifecycleawarecomponent.lca_component;
+
+import android.content.Context;
+
+public class MyComponent  {
+
+    private Context mContext;
+
+    public MyComponent(Context mContext) {
+        this.mContext = mContext;
+    }
+}
